214e05149638791c540d89eea66cb2f7a5330a55
harshparab <harsh_parab@outlook.com>
2023-08-21 22:24:49 +0530

    added deployment changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
